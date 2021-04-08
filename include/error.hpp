a5fded7fa148ff3a20faf8188b40ec1a20ad1648
Christian Kurz <chrikuvellberg@gmail.com>
2021-04-08 11:39:20 +0200

    gateway error constant

--- include/error.hpp
+++ include/error.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 namespace cbus {
-  enum class error_code : uint8_t { illegal_function = 1, illegal_data_address = 2 };
+  enum class error_code : uint8_t { illegal_function = 1, illegal_data_address = 2, gateway_no_response = 11 };
 }
