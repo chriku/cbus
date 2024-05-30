07313b3089e1aac92d179087d2c5f09c0851a9de
Christian Kurz <chrikuvellberg@gmail.com>
2024-05-30 23:45:29 +0200

    fill error code list

--- include/error.hpp
+++ include/error.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
 namespace cbus {
-  enum class error_code : uint8_t { illegal_function = 1, illegal_data_address = 2, gateway_no_response = 11 };
+  enum class error_code : uint8_t {
+    //
+    illegal_function = 1,
+    illegal_data_address = 2,
+    illegal_data_value = 3,
+    slave_device_failure = 4,
+    acknowledge = 5,
+    slave_device_busy = 6,
+    negative_acknowledge = 7,
+    memory_parity_error = 8,
+    gateway_path_unavailable = 10,
+    gateway_no_response = 11
+  };
 }
